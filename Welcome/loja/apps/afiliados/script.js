9abf42c5f43fe7ca6cbbaa0ee496635b371cacf6
ALPHATV <alpha.tech.pvai@gmail.com>
2026-02-28 22:18:56 -0300

    SDF

---
+++ Welcome/loja/apps/afiliados/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* ================= VIDEO AUTO PLAY ================= */
+
+  const video = document.getElementById("bgVideo");
+  if (video) {
+    video.play().catch(() => {});
+  }
+
+  /* ================= MENSAGENS DINÂMICAS ================= */
+
+  const messages = [
+    "Conectando aos servidores...",
+    "Verificando segurança...",
+    "Carregando catálogo...",
+    "Preparando o painel...",
+    "Entrando na plataforma...",
+	"Seja bem vindo..."
+  ];
+
+  let i = 0;
+  const statusText = document.getElementById("statusText");
+
+  const interval = setInterval(() => {
+    i++;
+    if (i < messages.length) {
+      statusText.textContent = messages[i];
+    }
+  }, 1000);
+
+  /* ================= REDIRECIONAMENTO ================= */
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    // efeito leve antes de sair
+    document.body.style.opacity = "0";
+    document.body.style.transition = "opacity 0.5s ease";
+
+    setTimeout(() => {
+      window.location.href = "https://alphatv-painel.netlify.app/";
+    }, 500);
+
+  }, 5000);
+
+});
